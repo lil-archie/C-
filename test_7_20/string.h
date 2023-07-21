782074f930e7acb141ae1d02e560ba46dac186ac
lil-archie <108964143+lil-archie@users.noreply.github.com>
2023-07-21 20:46:51 +0800

    Add files via upload

--- test_7_20/string.h
+++ test_7_20/string.h
@@ -25,6 +25,14 @@ namespace mystring
 			strcpy(_str, str);
 		}
 
+		string(const string& str) //拷贝构造函数
+		{
+			_str = new char[str._capacity + 1];
+			strcpy(_str, str._str);
+			_size = str._size;
+			_capacity = str._capacity;
+		}
+
 		~string()//析构函数
 		{
 			delete[] _str;
@@ -105,11 +113,152 @@ namespace mystring
 			assert(pos < _size);
 			if (n + _size >= _capacity)
 				reserve(n + _size + 2);
+			int end = _size;
+			//挪动数据
+			while ((int)pos <= end )//当end减小到最小时会转换成很大的数，这里会隐式转换。//while(pos<=end&&end!=npos)
+			{
+				_str[end + n] = _str[end];
+				end--;
+			}
+			for (int i = 0; i < n; i++)
+			{
+				_str[pos+i] = ch;
+			}
+			_size += n;
+		}
+
+		void insert(size_t pos, const char* str)
+		{
+			assert(pos < _size);
+			size_t len = strlen(str);
 			size_t end = _size;
+			if (len + _size > _capacity)
+			{
+				reserve(len + _size + 2);
+			}
 			while (pos <= end && end != npos)
 			{
+				_str[end + len] = _str[end];
+				end--;
+			}
+			for (int i = 0; i < len; i++)
+			{
+				_str[pos + i] = str[i];
 
 			}
+			_size += len;
+		}
+
+		typedef char* iterator;
+		typedef const char* const_iterator;
+
+		iterator end()
+		{
+			return _str + _size;
+		}
+
+		iterator begin()
+		{
+			return _str;
+		}
+
+		const_iterator end() const 
+		{
+			return _str + _size;
+		}
+
+		const_iterator begin() const 
+		{
+			return _str;
+		}
+
+
+		void erase(size_t pos, size_t len = npos)
+		{
+			assert(pos <= _size);
+			if (len == npos || len + pos >= _size)
+			{
+				_str[pos] = '\0';
+				_size = pos;
+				_str[_size] = '\0';
+			}
+			else
+			{
+				size_t end = len +pos;
+				while (end <= _size)
+				{
+					_str[pos++] = _str[end++];
+				}
+				_size -= len;
+
+			}
+
+		}
+
+
+		size_t find(char ch, size_t pos=npos)
+		{
+			assert(pos <= _size);
+			for (int i = pos; i < _size; i++)
+			{
+				if (ch == _str[i])
+					return i;
+			}
+			return npos;
+
+		}
+		
+		size_t find(const char* str, size_t pos=0)
+		{
+			assert(pos < _size);
+			const char* ptr = strstr(_str + pos, str);
+			if (ptr)
+			{
+				return ptr - _str;
+			}
+			else
+				return npos;
+			
+		}
+
+		/*string substr(size_t pos=0, size_t len=npos)
+		{
+			assert(pos <= _size);
+			size_t n=len;
+			if (len == npos || pos + len >= _size)
+			{
+				 n = _size - pos;
+			}
+			string tmp;
+			reserve(n);
+			for (int i = 0; i < n; i++)
+			{
+				tmp += _str[pos + i];
+			}
+			return tmp;
+
+			
+			
+		}*/
+
+		string substr(size_t pos = 0, size_t len = npos)
+		{
+			assert(pos < _size);
+
+			size_t n = len;
+			if (len == npos || pos + len > _size)
+			{
+				n = _size - pos;
+			}
+
+			string tmp;
+			tmp.reserve(n+2);
+			for (size_t i = pos; i < pos + n; i++)
+			{
+				tmp += _str[i];
+			}
+
+			return tmp;
 		}
 
 		static const size_t npos;
@@ -122,5 +271,5 @@ namespace mystring
 		char* _str;
 	};
 
-	const size_t string::npos = -1;s
+	const size_t string::npos = -1;
 }
--- test_7_20/test.cpp
+++ test_7_20/test.cpp
@@ -1,9 +1,9 @@
 #define  _CRT_SECURE_NO_WARNINGS 1
 #include"string.h"
 
-void Test1()
-{
-	mystring::string s1("hello world");
+//void Test1()
+//{
+	//mystring::string s1("hello world");
 	/*cout << s1.c_str() << endl;
 	cout << s1.size() << endl;
 	cout << s1[0] << endl;
@@ -18,31 +18,121 @@ void Test1()
 
 	/*s1.append("hello bit");
 	cout << s1.c_str()<< endl;*/
-}
+//}
 
-void Test2()
+//void Test2()
+//{
+//	mystring::string s1("hello world");
+//	mystring::string s2("hello bit");
+//
+//	string s3("hello world");
+//	string s4("hello");
+//
+//	s3 += s4;
+//	cout << s3 << endl;
+//	
+//	s1.append("append");
+//
+//	s1 += "hello world";
+//	s1 += 'x';
+//
+//	s1.insert(0, 3, 'c');
+//
+//	s1.insert(2, 2, 'v');
+//
+//	s1.insert(2, "hello");
+//
+//	cout << s1.c_str() << endl;
+//
+//
+//}
+
+//void Test3()
+//{
+//	mystring::string s1("hello world");
+//	/*mystring::string::iterator it = s1.begin();
+//	while (it != s1.end())
+//	{
+//		cout << *it << " ";
+//		it++;
+//	}*/
+//	mystring::string s2("hellobit");
+//
+//	
+//	cout << s2.find('h', 0) << endl;
+//
+//	cout << s1.find('lo', 0) << endl;
+//	
+//
+//	/*for (auto ch : s2)
+//	{
+//		cout << ch << " ";
+//	}
+//
+//	cout << endl;
+//
+//	s1.erase(2,3);
+//
+//	for (auto ch : s1)
+//	{
+//		cout << ch << " ";
+//	}*/
+//	cout << endl;
+//
+//	/*const mystring::string s2("hello");
+//
+//	s1
+//
+//	for (auto ch : s2)
+//	{
+//		cout << ch << " ";
+//	}*/
+//}
+
+
+void test_string5()
 {
-	mystring::string s1("hello world");
-	mystring::string s2("hello bit");
+	// 21£º18¼ÌÐø
+	mystring::string url = "ftp://www.baidu.com/?tn=65081411_1_oem_dg";
 
-	string s3("hello world");
-	string s4("hello");
-
-	s3 += s4;
-	cout << s3 << endl;
+	size_t pos1 = url.find("://");
+	if (pos1 != mystring::string::npos)
+	{
+		mystring::string url = "ftp://www.baidu.com/?tn=65081411_1_oem_dg";
 	
-	s1.append("append");
+		
+		mystring::string protocol = url.substr(0, pos1);
+		cout << protocol.c_str() << endl;
+	}
 
-	s1 += "hello world";
-	s1 += 'x';
+	size_t pos2 = url.find('/', pos1 + 3);
+	if (pos2 != mystring::string::npos)
+	{
+		mystring::string domain = url.substr(pos1 + 3, pos2 - (pos1 + 3));
+		mystring::string uri = url.substr(pos2 + 1);
 
-	cout << s1.c_str() << endl;
-
-
+		cout << domain.c_str() << endl;
+		cout << uri.c_str() << endl;
+	}
 }
 
+//void Test4()
+//{
+//	mystring::string s1("Test string");
+//
+//
+//	mystring::string s2=s1.substr(0,2);
+//
+//	cout << s2.c_str() << endl;
+//
+//	
+//}
 int main()
 {
 	//Test1();
-	Test2();
+	//Test2();
+	//Test3();
+	 test_string5();
+	//Test4();
+
 }
