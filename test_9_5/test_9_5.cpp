3b78e5bb783d3144d413619c622c8af22ff8b914
lil-archie <108964143+lil-archie@users.noreply.github.com>
2023-09-05 22:45:11 +0800

    Add files via upload

---
+++ test_9_5/test_9_5.cpp
@@ -0,0 +1,157 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+
+#pragma once
+
+#include<iostream>
+using namespace std;
+
+template<class K, class V>
+struct AVLTreeNode
+{
+	pair<K, V> _kv;
+	AVLTreeNode<K, V>* _left;
+	AVLTreeNode<K, V>* _right;
+	AVLTreeNode<K, V>* _parent;
+	int _bf;
+
+	AVLTreeNode(const pair<K, V>& kv)
+		:_kv(kv)
+		, _left(nullptr)
+		, _right(nullptr)
+		, _parent(nullptr)
+		, _bf(0)
+	{}
+};
+
+template<class K, class V>
+class AVLTree
+{
+	typedef AVLTreeNode<K, V> Node;
+public:
+	bool Insert(const pair<K, V>& kv)
+	{
+		if (_root == nullptr)
+		{
+			_root = new Node(kv);
+			return true;
+		}
+
+		Node* cur = _root;
+		while (cur)
+		{
+			if (cur->_kv.first < kv.first)
+			{
+				parent = cur;
+				cur = cur->_right;
+			}
+			else if (cur->_kv.first > kv.first)
+			{
+				parent = cur;
+				cur = cur->_left;
+			}
+			else
+			{
+				return false;
+			}
+		}
+
+		cur = new Node(kv);
+		if (parent->_kv.first < kv.first)
+		{
+			parent->_right = cur;
+		}
+		else
+		{
+			parent->_left = cur;
+		}
+
+		cur->_parent = parent;
+
+		// ... 控制平衡
+		// 更新平衡因子
+		while (parent)
+		{
+			if (cur == parent->_left)
+			{
+				parent->_bf--;
+			}
+			else // if (cur == parent->_right)
+			{
+				parent->_bf++;
+			}
+
+			if (parent->_bf == 0)
+			{
+				// 更新结束
+				break;
+			}
+			else if (parent->_bf == 1 || parent->_bf == -1)
+			{
+				// 继续往上更新
+				cur = parent;
+				parent = parent->_parent;
+			}
+			else if (parent->_bf == 2 || parent->_bf == -2)
+			{
+				// 子树不平衡了，需要旋转
+				if (parent->_bf == 2 || cur->bf == 1)
+				{
+					RotateL(parent);
+				}
+
+				break;
+			}
+			else
+			{
+				assert(false);
+			}
+		}
+
+
+		return true;
+	}
+
+	void RotateL(Node* parent)
+	{
+		Node* cur = parent->_right;
+		Node* curleft = cur->_left;
+
+		parent->_right = curleft;
+		if (curleft)
+		{
+			curleft->_parent = parent;
+		}
+
+		cur->_left = parent;
+
+		Node* ppnode = parent->_parent;
+
+		parent->_parent = cur;
+
+
+		if (parent == _root)
+		{
+			_root = cur;
+			cur->_parent = nullptr;
+		}
+		else
+		{
+			if (ppnode->_left == parent)
+			{
+				ppnode->_left = cur;
+			}
+			else
+			{
+				ppnode->_right = cur;
+
+			}
+
+			cur->_parent = ppnode;
+		}
+
+		parent->_bf = cur->_bf = 0;
+	}
+
+private:
+	Node* _root = nullptr;
+};
